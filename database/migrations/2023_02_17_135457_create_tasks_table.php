6d1dbcbd406c776c43cdb2178483bec745157f1e
João Pedro Gomes <86388032+JP-dev99@users.noreply.github.com>
2023-02-17 14:44:36 -0300

    feat: :card_file_box: Adding migrations

--- database/migrations/2023_02_17_135457_create_tasks_table.php
+++ database/migrations/2023_02_17_135457_create_tasks_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->boolean('is_admin');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('name');
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('task_group_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 };
